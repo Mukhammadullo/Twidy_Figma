73e7d69ff4772c4f6eb4f0ebc68699a5f6aea615
Muhammadullo <mnastulloev@gmail.com>
2023-12-04 10:23:07 +0500

    card_music

---
+++ page_music/music.js
@@ -0,0 +1,41 @@
+const API1 = "http://localhost:3000/data1"
+let container_data = document.querySelector(".container_data")
+
+
+// get 
+async function getData() {
+    try {
+        let { data } = await axios.get(API1)
+        console.log(data);
+        get(data)
+    } catch (error) {
+        console.error(error);
+    }
+}
+getData()
+
+
+// get
+function get(newData) {
+    newData.forEach(element => {
+
+        let forId = document.createElement("h4")
+        forId.innerHTML = element.id
+
+        let forAvatar = document.createElement("img")
+        forAvatar.src = element.avatar
+
+        let forName = document.createElement("h1")
+        forName.innerHTML = element.name
+
+        let card = document.createElement("div")
+
+        card.append(forId, forAvatar, forName)
+        card.classList.add("card")
+        
+            container_data.append(card)
+
+    });
+}
+
+
